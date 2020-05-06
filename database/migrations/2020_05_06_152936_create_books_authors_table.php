43d4cdb0a05a2e8692337a9db7826ddee904a224
kolosov-andrey <andrey.kolosov@intellicagroup.com>
2020-05-06 18:34:08 +0300

    make migrations

---
+++ database/migrations/2020_05_06_152936_create_books_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books_authors', function (Blueprint $table) {
+            $table->bigInteger('book_id')->unsigned()->index();
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->bigInteger('author_id')->unsigned()->index();
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+            $table->primary(['book_id', 'author_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books_authors');
+    }
+}
